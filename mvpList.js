3d147a6859900ac81947ad899624fa680b586037
Samudra Perera <samudrapup@gmail.com>
2022-08-19 00:37:43 -0400

    Created new file for APi Object and updated server.js

---
+++ mvpList.js
@@ -0,0 +1,183 @@
+const mvpList = {
+    '2000': {
+        'name': 'Allen Iverson',
+        'age': 25,
+        'ppg': 31.1,
+        'rpg': 3.8,
+        'apg': 4.6,
+        'status': 'retired'
+    },
+    '2001': {
+        'name': 'Tim Duncan',
+        'age': 25,
+        'ppg': 25.5,
+        'rpg': 12.7,
+        'apg': 3.7,
+        'status': 'retired'
+    },
+    '2002': {
+        'name': 'Tim Duncan',
+        'age': 26,
+        'ppg': 23.3,
+        'rpg': 12.9,
+        'apg': 3.9,
+        'status': 'retired'
+    },
+    '2003': {
+        'name': 'Kevin Garnett',
+        'age': 27,
+        'ppg': 24.2,
+        'rpg': 13.9,
+        'apg': 5.0,
+        'status': 'retired'
+    },
+    '2004': {
+        'name': 'Steve Nash',
+        'age': 30,
+        'ppg': 15.5,
+        'rpg': 3.3,
+        'apg': 11.5,
+        'status': 'retired'
+    },
+    '2005': {
+        'name': 'Steve Nash',
+        'age': 31,
+        'ppg': 18.8,
+        'rpg': 4.2,
+        'apg': 10.5,
+        'status': 'retired'
+    },
+    '2006': {
+        'name': 'Dirk Nowitzki',
+        'age': 28,
+        'ppg': 24.6,
+        'rpg': 8.9,
+        'apg': ``,
+        'status': 'retired'
+    },
+    '2007': {
+        'name': 'Kobe Bryant',
+        'age': 29,
+        'ppg': 28.3,
+        'rpg': 6.3,
+        'apg': 5.4,
+        'status': 'retired'
+    },
+    '2008': {
+        'name': 'Lebron James',
+        'age': 24,
+        'ppg': 28.4,
+        'rpg': 7.6,
+        'apg': 7.2,
+        'status': 'Active'
+    },
+    '2009': {
+        'name': 'Lebron James',
+        'age': 25,
+        'ppg': 29.7,
+        'rpg': 7.3,
+        'apg': 8.6,
+        'status': 'Active'
+    },
+    '2010': {
+        'name': 'Derrick Rose',
+        'age': 26,
+        'ppg': 25.0,
+        'rpg': 4.1,
+        'apg': 7.7,
+        'status': 'Active'
+    },
+    '2011': {
+        'name': 'Lebron James',
+        'age': 27,
+        'ppg': 27.1,
+        'rpg': 7.9,
+        'apg': 6.2,
+        'status': 'Active'
+    },
+    '2012': {
+        'name': 'Lebron James',
+        'age': 28,
+        'ppg': 26.8,
+        'rpg': 8.0,
+        'apg': 7.3,
+        'status': 'Active'
+    },
+    '2013': {
+        'name': 'Kevin Durant',
+        'age': 25,
+        'ppg': 32.0,
+        'rpg': 7.4,
+        'apg': 5.5,
+        'status': 'Active'
+    },
+    '2014': {
+        'name': 'Stephen Curry',
+        'age': 26,
+        'ppg': 23.8,
+        'rpg': 4.3,
+        'apg': 7.7,
+        'status': 'Active'
+    },
+    '2015': {
+        'name': 'Stephen Curry',
+        'age': 27,
+        'ppg': 30.1,
+        'rpg': 5.4,
+        'apg': 6.7,
+        'status': 'Active'
+    },
+    '2016': {
+        'name': 'Russell WestBrook',
+        'age': 28,
+        'ppg': 31.6,
+        'rpg': 10.7,
+        'apg': 10.4,
+        'status': 'Active'
+    },
+    '2017': {
+        'name': 'James Harden',
+        'age': 28,
+        'ppg': 30.4,
+        'rpg': 5.4,
+        'apg': 8.8,
+        'status': 'Active'
+    },
+    '2018': {
+        'name': 'Giannis Antetokounmpo',
+        'age': 24,
+        'ppg': 27.7,
+        'rpg': 12.5,
+        'apg': 5.9,
+        'status': 'Active'
+    },
+    '2019': {
+        'name': 'Giannis Antetokounmpo',
+        'age': 25,
+        'ppg': 29.5,
+        'rpg': 13.6,
+        'apg': 5.6,
+        'status': 'Active'
+    },
+    '2020': {
+        'name': 'Nikola Jokic',
+        'age': 25,
+        'ppg': 26.4,
+        'rpg': 10.8,
+        'apg': 8.3,
+        'status': 'Active'
+    },
+    '2021': {
+        'name': 'Nikola Jokic',
+        'age': 26,
+        'ppg': 27.1,
+        'rpg': 13.8,
+        'apg': 7.9,
+        'status': 'Active'
+    },
+    'error': {
+        'error': 'Enter a valid year'
+    }
+}
+
+module.exports = { mvpList }
